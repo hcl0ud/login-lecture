0525b05ed1350c3644c11e580a66bf1d5e2ec99a
cl0ud <148cl0ud@gmail.com>
2022-01-26 20:20:47 +0900

    백엔드에서 로그인 기능 구현

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -15,8 +15,10 @@ function login() {
     fetch("/login", {
         method: "POST",
         headers: {
-            "Content=Type": "application/json"
+            "Content-Type": "application/json",
         },
         body: JSON.stringify(req),
-    });
+    })
+        .then((res) => console.log(res.json()))
+        .then((res) => { });
 }
